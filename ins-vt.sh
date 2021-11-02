9ef759a439a9c00a43fac716712ad68aa549cfbf
haxoscript <43457466+haxoscript@users.noreply.github.com>
2021-11-02 22:19:25 +0700

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -15,10 +15,8 @@ chronyc sourcestats -v
 chronyc tracking -v
 date
 
-mkdir -p /etc/trojan/
-touch /etc/trojan/akun.conf
 # install v2ray
-wget https://github.com/haxoscript/ppqiu/raw/main/go.sh && chmod +x go.sh && ./go.sh
+wget https://raw.githubusercontent.com/haxoscript/ppqwiu/main/go.sh && chmod +x go.sh && ./go.sh
 rm -f /root/go.sh
 bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
 mkdir /root/.acme.sh
@@ -37,7 +35,7 @@ cat> /etc/v2ray/config.json << END
   },
   "inbounds": [
     {
-      "port": 444,
+      "port": 443,
       "protocol": "vmess",
       "settings": {
         "clients": [
@@ -134,7 +132,7 @@ cat> /etc/v2ray/none.json << END
   },
   "inbounds": [
     {
-      "port": 445,
+      "port": 80,
       "protocol": "vmess",
       "settings": {
         "clients": [
@@ -213,267 +211,12 @@ cat> /etc/v2ray/none.json << END
   }
 }
 END
-cat> /etc/v2ray/vless.json << END
-{
-  "log": {
-    "access": "/var/log/v2ray/access2.log",
-    "error": "/var/log/v2ray/error.log",
-    "loglevel": "info"
-  },
-  "inbounds": [
-    {
-      "port": 446,
-      "protocol": "vless",
-      "settings": {
-        "clients": [
-          {
-            "id": "${uuid}"
-#tls
-          }
-        ],
-        "decryption": "none"
-      },
-      "streamSettings": {
-        "network": "ws",
-        "security": "tls",
-        "tlsSettings": {
-          "certificates": [
-            {
-              "certificateFile": "etc/v2ray/v2ray.crt",
-              "keyFile": "/etc/v2ray/v2ray.key"
-            }
-          ]
-        },
-        "wsSettings": {
-          "path": "/sshinjector",
-          "headers": {
-            "Host": ""
-          }
-         },
-        "quicSettings": {},
-        "sockopt": {
-          "mark": 0,
-          "tcpFastOpen": true
-        }
-      },
-      "sniffing": {
-        "enabled": true,
-        "destOverride": [
-          "http",
-          "tls"
-        ]
-      }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    },
-    {
-      "protocol": "blackhole",
-      "settings": {},
-      "tag": "blocked"
-    }
-  ],
-  "routing": {
-    "rules": [
-      {
-        "type": "field",
-        "ip": [
-          "0.0.0.0/8",
-          "10.0.0.0/8",
-          "100.64.0.0/10",
-          "169.254.0.0/16",
-          "172.16.0.0/12",
-          "192.0.0.0/24",
-          "192.0.2.0/24",
-          "192.168.0.0/16",
-          "198.18.0.0/15",
-          "198.51.100.0/24",
-          "203.0.113.0/24",
-          "::1/128",
-          "fc00::/7",
-          "fe80::/10"
-        ],
-        "outboundTag": "blocked"
-      },
-      {
-        "type": "field",
-        "outboundTag": "blocked",
-        "protocol": [
-          "bittorrent"
-        ]
-      }
-    ]
-  }
-}
-END
-cat> /etc/v2ray/vnone.json << END
-{
-  "log": {
-    "access": "/var/log/v2ray/access2.log",
-    "error": "/var/log/v2ray/error.log",
-    "loglevel": "info"
-  },
-  "inbounds": [
-    {
-      "port": 447,
-      "protocol": "vless",
-      "settings": {
-        "clients": [
-          {
-            "id": "${uuid}"
-#none
-          }
-        ],
-        "decryption": "none"
-      },
-      "streamSettings": {
-        "network": "ws",
-        "wsSettings": {
-          "path": "/sshinjector",
-          "headers": {
-            "Host": ""
-          }
-         },
-        "quicSettings": {},
-        "sockopt": {
-          "mark": 0,
-          "tcpFastOpen": true
-        }
-      },
-      "sniffing": {
-        "enabled": true,
-        "destOverride": [
-          "http",
-          "tls"
-        ]
-      },
-      "domain": "$domain"
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    },
-    {
-      "protocol": "blackhole",
-      "settings": {},
-      "tag": "blocked"
-    }
-  ],
-  "routing": {
-    "rules": [
-      {
-        "type": "field",
-        "ip": [
-          "0.0.0.0/8",
-          "10.0.0.0/8",
-          "100.64.0.0/10",
-          "169.254.0.0/16",
-          "172.16.0.0/12",
-          "192.0.0.0/24",
-          "192.0.2.0/24",
-          "192.168.0.0/16",
-          "198.18.0.0/15",
-          "198.51.100.0/24",
-          "203.0.113.0/24",
-          "::1/128",
-          "fc00::/7",
-          "fe80::/10"
-        ],
-        "outboundTag": "blocked"
-      },
-      {
-        "type": "field",
-        "outboundTag": "blocked",
-        "protocol": [
-          "bittorrent"
-        ]
-      }
-    ]
-  }
-}
-END
-cat <<EOF > /etc/trojan/config.json
-{
-    "run_type": "server",
-    "local_addr": "0.0.0.0",
-    "local_port": 443,
-    "remote_addr": "127.0.0.1",
-    "remote_port": 2603,
-    "password": [
-        "$uuid"
-    ],
-    "log_level": 1,
-    "ssl": {
-        "cert": "/etc/v2ray/v2ray.crt",
-        "key": "/etc/v2ray/v2ray.key",
-        "key_password": "",
-        "cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384",
-        "cipher_tls13": "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
-        "prefer_server_cipher": true,
-        "alpn": [
-            "http/1.1"
-        ],
-        "reuse_session": true,
-        "session_ticket": false,
-        "session_timeout": 600,
-        "plain_http_response": "",
-        "curves": "",
-        "dhparam": ""
-    },
-    "tcp": {
-        "prefer_ipv4": false,
-        "no_delay": true,
-        "keep_alive": true,
-        "reuse_port": false,
-        "fast_open": false,
-        "fast_open_qlen": 20
-    },
-    "mysql": {
-        "enabled": false,
-        "server_addr": "127.0.0.1",
-        "server_port": 3306,
-        "database": "trojan",
-        "username": "trojan",
-        "password": "",
-        "key": "",
-        "cert": "",
-        "ca": ""
-    }
-}
-EOF
-cat <<EOF> /etc/systemd/system/trojan.service
-[Unit]
-Description=Trojan
-Documentation=https://trojan-gfw.github.io/trojan/
-[Service]
-Type=simple
-ExecStart=/usr/local/bin/trojan -c /etc/trojan/config.json -l /var/log/trojan.log
-Type=simple
-KillMode=process
-Restart=no
-RestartSec=42s
-[Install]
-WantedBy=multi-user.target
-EOF
-cat <<EOF > /etc/trojan/uuid.txt
-$uuid
-EOF
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 444 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 445 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 446 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 447 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
 
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 444 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 445 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 446 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 447 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+
 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 80 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 iptables-restore -t < /etc/iptables.up.rules
 netfilter-persistent save
@@ -481,49 +224,21 @@ netfilter-persistent reload
 systemctl daemon-reload
 systemctl enable v2ray@none.service
 systemctl start v2ray@none.service
-systemctl enable v2ray@vless.service
-systemctl start v2ray@vless.service
-systemctl enable v2ray@vnone.service
-systemctl start v2ray@vnone.service
-systemctl restart trojan
-systemctl enable trojan
 systemctl restart v2ray
 systemctl enable v2ray
 cd /usr/bin
-wget -O addws "https://github.com/haxoscript/ppqiu/raw/main/addws.sh"
-wget -O addvless "https://github.com/haxoscript/ppqiu/raw/main/addvless.sh"
-wget -O addtr "https://github.com/haxoscript/ppqiu/raw/main/addtr.sh"
-wget -O delws "https://github.com/haxoscript/ppqiu/raw/main/delws.sh"
-wget -O delvless "https://servip.net/ppq/delvless.sh"
-wget -O deltr "https://github.com/haxoscript/ppqiu/raw/main/deltr.sh"
-wget -O cekws "https://github.com/haxoscript/ppqiu/raw/main/cekws.sh"
-wget -O cekvless "https://github.com/haxoscript/ppqiu/raw/main/cekvless.sh"
-wget -O cektr "https://github.com/haxoscript/ppqiu/raw/main/cektr.sh"
-wget -O renewws "https://github.com/haxoscript/ppqiu/raw/main/renewws.sh"
-wget -O renewvless "https://github.com/haxoscript/ppqiu/raw/main/renewvless.sh"
-wget -O renewtr "https://github.com/haxoscript/ppqiu/raw/main/renewtr.sh"
-wget -O xp-ws "https://github.com/haxoscript/ppqiu/raw/main/xp-ws.sh"
-wget -O xp-tr "https://github.com/haxoscript/ppqiu/raw/main/xp-tr.sh"
-wget -O xp-vless "https://github.com/haxoscript/ppqiu/raw/main/xp-vless.sh"
-wget -O certv2ray "https://github.com/haxoscript/ppqiu/raw/main/cert.sh"
+wget -O addws "https://raw.githubusercontent.com/haxoscript/ppqwiu/main/addws.sh"
+wget -O delws "https://raw.githubusercontent.com/haxoscript/ppqwiu/main/delws.sh"
+wget -O cekws "https://raw.githubusercontent.com/haxoscript/ppqwiu/main/cekws.sh"
+wget -O renewws "https://raw.githubusercontent.com/haxoscript/ppqwiu/main/renewws.sh"
+wget -O xp-ws "https://raw.githubusercontent.com/haxoscript/ppqwiu/main/xp-ws.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/haxoscript/ppqwiu/main/cert.sh"
 chmod +x addws
-chmod +x addvless
-chmod +x addtr
 chmod +x delws
-chmod +x delvless
-chmod +x deltr
 chmod +x cekws
-chmod +x cekvless
-chmod +x cektr
 chmod +x renewws
-chmod +x renewtr
-chmod +x renewvless
 chmod +x xp-ws
-chmod +x xp-tr
-chmod +x xp-vless
 chmod +x certv2ray
 cd
 mv /root/domain /etc/v2ray
 echo "0 0 * * * root xp-ws" >> /etc/crontab
-echo "0 0 * * * root xp-tr" >> /etc/crontab
-echo "0 0 * * * root xp-vless" >> /etc/crontab
